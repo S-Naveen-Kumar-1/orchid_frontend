dc3f80f50eac59584bfc9e6a95c58b5f79a9116f
CNT-Mac-004 <navee1110@gmail.com>
2026-06-28 11:28:57 +0530

    changes made

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
-export const BASE_URL = 'http://localhost:8000';
+// export const BASE_URL = 'http://localhost:8000';
 
-// export const BASE_URL = 'https://orchid-service.onrender.com';
+export const BASE_URL = 'https://orchid-service.onrender.com';
 
 
 // export const BASE_URL = 'http://10.0.2.2:8000/';
